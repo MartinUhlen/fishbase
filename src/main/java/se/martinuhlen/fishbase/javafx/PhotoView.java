318ad465380f106e9a82deafc002c60f518d011e
Martin Uhlén <martin.uhlen@gmail.com>
2018-04-13 13:34:14 +0200

    Load photos in background thread and show progress animation while loading.

--- src/main/java/se/martinuhlen/fishbase/javafx/PhotoView.java
+++ src/main/java/se/martinuhlen/fishbase/javafx/PhotoView.java
@@ -1,8 +1,18 @@
 package se.martinuhlen.fishbase.javafx;
 
+import static java.util.Collections.emptySet;
+import static javafx.scene.layout.Region.USE_PREF_SIZE;
+
+import java.util.List;
+
 import javafx.beans.property.ReadOnlyStringProperty;
 import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.scene.Node;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.StackPane;
+import se.martinuhlen.fishbase.drive.photo.FishingPhoto;
 import se.martinuhlen.fishbase.drive.photo.PhotoService;
 import se.martinuhlen.fishbase.javafx.action.Action;
 import se.martinuhlen.fishbase.javafx.action.RunnableAction;
@@ -14,18 +24,27 @@ public class PhotoView implements View
 	private final RunnableAction refreshAction = new RunnableAction(true, () -> refresh());
 
 	private final PhotoService service;
+	private final StackPane stackPane;
 	private final ThumbnailPane thumbnailPane;
+	private final ProgressIndicator progressIndicator;
+	private final PhotoLoader photoLoader;
 
 	public PhotoView(PhotoService service)
 	{
 		this.service = service;
 		this.thumbnailPane = ThumbnailPane.forTimeline();
+		this.progressIndicator = new ProgressIndicator();
+		this.stackPane = new StackPane(thumbnailPane, progressIndicator);
+		this.photoLoader = new PhotoLoader();
 	}
 
 	@Override
 	public Node getContent()
 	{
-		return thumbnailPane;
+	    progressIndicator.prefHeightProperty().bind(stackPane.heightProperty().multiply(0.3));
+	    progressIndicator.prefWidthProperty().bind(progressIndicator.heightProperty());
+	    progressIndicator.setMaxSize(USE_PREF_SIZE, USE_PREF_SIZE);
+		return stackPane;
 	}
 
 	@Override
@@ -36,12 +55,37 @@ public class PhotoView implements View
 
 	private void refresh()
 	{
-		thumbnailPane.setPhotos(service.getFishingPhotos());
+	    photoLoader.restart();
 	}
 
 	@Override
 	public ReadOnlyStringProperty titleProperty()
 	{
 		return titleProperty;
+	}
+
+	private class PhotoLoader extends Service<List<FishingPhoto>>
+	{
+        @Override
+        protected Task<List<FishingPhoto>> createTask()
+        {
+            thumbnailPane.setPhotos(emptySet());
+            progressIndicator.setVisible(true);
+            return new Task<>()
+            {
+                @Override
+                protected List<FishingPhoto> call() throws Exception
+                {
+                    return service.getFishingPhotos();
+                }
+            };
+        }
+
+        @Override
+        protected void succeeded()
+        {
+            progressIndicator.setVisible(false);
+            thumbnailPane.setPhotos(getValue());
+        }
 	}
 }
